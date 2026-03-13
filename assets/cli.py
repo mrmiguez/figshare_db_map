5203092483368efa5b3ab32ee3a0877821a5b811
Matthew <r.m.miguez@gmail.com>
2026-03-13 14:31:37 -0700

    Begin scaffold

---
+++ assets/cli.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import argparse
+
+PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+def argument_parser():
+    '''Top level argument parser'''
+    args = argparse.ArgumentParser(description='Figshare data-mapping DB utility')
+    args.add_argument('-s', '--status', help='get DB status', action='store_true')
+    args.add_argument('-v', '--verbose', action='store_true')
+    args.add_argument('-b', '--burndown', action='store_true', help='burndown database')
+    run_args_group = args.add_argument_group('Run data mapping')
+    run_args_group.add_argument('-r', '--run', action='store_true')
+    run_args_group.add_argument('records', help='path to XML records', default=PATH)
+
+    # hack to show help when no arguments supplied
+    if len(sys.argv) == 1:
+        args.print_help()
+        sys.exit(0)
+
+    return args
---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import pymods
+import assets
+import logging
+import argparse
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(PATH, 'figshare_record_tables.sqlite3')
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+if __name__ == '__main__':
+    db_conn = assets.connect_db(DB_PATH)
+    args = assets.argument_parser().parse_args()
+    print(args)  #test
+
+    if args.status:
+        print(assets.get_db_status(db_conn))
+
+    if args.burndown:
+        os.remove(DB_PATH)
+
