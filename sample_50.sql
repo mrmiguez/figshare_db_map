e43f09dc6fd6c90b67abb9764c0d0351281a081a
Matthew <r.m.miguez@gmail.com>
2026-06-30 15:02:21 -0700

    QA SQL

---
+++ sample_50.sql
@@ -0,0 +1,24 @@
+.headers on
+.mode tabs
+.once sample.tsv
+
+SELECT
+    o.*,
+    GROUP_CONCAT(
+        TRIM(
+            COALESCE(a.surname, '') ||
+            CASE WHEN a.firstname IS NOT NULL
+                 THEN ', ' || a.firstname
+                 ELSE ''
+            END
+        ),
+        '; '
+    ) AS authors
+FROM objects o
+LEFT JOIN object_authors oa
+    ON o.pid = oa.object_id
+LEFT JOIN authors a
+    ON oa.author_id = a.id
+GROUP BY o.pid
+ORDER BY RANDOM()
+LIMIT 50;
